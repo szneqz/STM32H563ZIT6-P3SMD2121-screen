6fd9e9a6f0198818112c0abd0b1d016cf448f6bf
szneqz <crashday3@o2.pl>
2026-06-07 17:51:12 +0200

    Create tetris logic

--- Core/Src/tetris.c
+++ Core/Src/tetris.c
@@ -6,9 +6,11 @@
  */
 
 #include "tetris.h"
+#include <string.h>
 
 #define TETRIS_WIDTH 10
 #define TETRIS_HEIGHT 16
+#define TETRIS_PIXEL_SIZE 160
 #define TETRIS_X00 43
 #define TETRIS_X01 62
 #define TETRIS_X10 65
@@ -24,16 +26,15 @@
 
 static ColorBitfield black = { 0x0000 };
 
-const unsigned long maxTetrisGameDelay = 30;
-unsigned long tetrisGameDelay = 0;
-const unsigned long maxMoveTetrisLeftRightDelay = 50;
-unsigned long moveTetrisLeftRightDelay = 0;
+static const unsigned long maxTetrisGameDelay = 30;
+static const unsigned long maxMoveTetrisLeftRightDelay = 200;
 
 static uint8_t GetFigureBlockPos(uint8_t i, int8_t myFigPosX, int8_t myFigPosY, int8_t myFigRot, int8_t thisFigure);
+static void DrawFigure(int8_t lastPosX, int8_t lastPosY, int8_t lastRot);
 static void DrawAnyFigure(int8_t myFigPosX, int8_t myFigPosY, int8_t myFigRot, int8_t thisFigure);
 static bool CheckFigurePossibility(int8_t myFigPosX, int8_t myFigPosY, int8_t myFigRot);
 static void RotateTetrisFigure(int8_t dir);
-static void MoveTetrisLeftRight(int8_t dir);
+static void MoveTetrisLeftRight(int8_t dir, uint32_t actualMillis);
 static void CheckWholeLines(int8_t minHeight, int8_t maxHeight);
 static void TETRIS_DrawBorder(void);
 static void TETRIS_DrawPixel(uint8_t x, uint8_t y, ColorBitfield hubColor, bool nokiaColor);
@@ -42,13 +43,15 @@ enum TETRIS_MODE {
 	TETRIS_DEAD, TETRIS_STATIC, TETRIS_PLAYING
 };
 
-int8_t figurePosX = 0;
-int8_t figurePosY = 0;
-const int8_t figurePosXStart = 3;
-int8_t figureRot = 0;  //4 rotations
+static ColorBitfield tetrisPlayfield[TETRIS_PIXEL_SIZE];
+
+static int8_t figurePosX = 0;
+static int8_t figurePosY = 0;
+static const int8_t figurePosXStart = 3;
+static int8_t figureRot = 0;  //4 rotations
 //cyan, 	red, 	  green, 	blue, 	  orange, 	magenta,  yellow
-ColorBitfield tetrisColors[TETRIS_NR_FIGURES] = { {0x03ff}, {0x7c00}, {0x03e0}, {0x001f}, {0x7e00}, {0x7c1f}, {0x7fe0} };
-int8_t figures[TETRIS_NR_FIGURES][4][4] = {
+static ColorBitfield tetrisColors[TETRIS_NR_FIGURES] = { {0x03ff}, {0x7c00}, {0x03e0}, {0x001f}, {0x7e00}, {0x7c1f}, {0x7fe0} };
+static int8_t figures[TETRIS_NR_FIGURES][4][4] = {
 		{ { 4, 5, 6, 7 }, { 2, 6, 10, 14 }, { 8, 9, 10, 11 }, { 1, 5, 9, 13 } },  // line
 		{ { 0, 1, 5, 6 }, { 2, 5, 6, 9 }, { 4, 5, 9, 10 }, { 1, 4, 5, 8 } },      // Z
 		{ { 1, 2, 4, 5 }, { 1, 5, 6, 10 }, { 5, 6, 8, 9 }, { 0, 4, 5, 9 } },      // Z reversed
@@ -57,43 +60,47 @@ int8_t figures[TETRIS_NR_FIGURES][4][4] = {
 		{ { 1, 4, 5, 6 }, { 1, 5, 6, 9 }, { 4, 5, 6, 9 }, { 1, 4, 5, 9 } },       // |-
 		{ { 1, 2, 5, 6 }, { 1, 2, 5, 6 }, { 1, 2, 5, 6 }, { 1, 2, 5, 6 } }        // square
 };
-const int8_t wallKicksAmount = 5;
-int8_t regularWallKicksClockwise[4][5][2] = {
+static const int8_t wallKicksAmount = 5;
+static int8_t regularWallKicksClockwise[4][5][2] = {
 		{ { 0, 0 }, { -1, 0 }, { -1, -1 }, { 0, 2 }, { -1, 2 } },  //0>>1
 		{ { 0, 0 }, { 1, 0 }, { 1, 1 }, { 0, -2 }, { 1, -2 } },    //1>>2
 		{ { 0, 0 }, { 1, 0 }, { 1, -1 }, { 0, 2 }, { 1, 2 } },     //2>>3
 		{ { 0, 0 }, { -1, 0 }, { -1, 1 }, { 0, -2 }, { -1, -2 } }  //3>>0
 };
-int8_t regularWallKicksCounterClockwise[4][5][2] = {
+static int8_t regularWallKicksCounterClockwise[4][5][2] = {
 		{ { 0, 0 }, { 1, 0 }, { 1, -1 }, { 0, 2 }, { 1, 2 } },     //0>>3
 		{ { 0, 0 }, { 1, 0 }, { 1, 1 }, { 0, -2 }, { 1, -2 } },    //1>>0
 		{ { 0, 0 }, { -1, 0 }, { -1, -1 }, { 0, 2 }, { -1, 2 } },  //2>>1
 		{ { 0, 0 }, { -1, 0 }, { -1, 1 }, { 0, -2 }, { -1, -2 } }  //3>>2
 };
-int8_t iWallKicksClockwise[4][5][2] = {
+static int8_t iWallKicksClockwise[4][5][2] = {
 		{ { 0, 0 }, { -2, 0 }, { 1, 0 }, { -2, 1 }, { 1, -2 } },  //0>>1
 		{ { 0, 0 }, { -1, 0 }, { 2, 0 }, { -1, -2 }, { 2, 1 } },  //1>>2
 		{ { 0, 0 }, { 2, 0 }, { -1, 0 }, { 2, -1 }, { -1, 2 } },  //2>>3
 		{ { 0, 0 }, { 1, 0 }, { -2, 0 }, { 1, 2 }, { -2, -1 } }   //3>>0
 };
-int8_t iWallKicksCounterClockwise[4][5][2] = {
+static int8_t iWallKicksCounterClockwise[4][5][2] = {
 		{ { 0, 0 }, { -1, 0 }, { 2, 0 }, { -1, -2 }, { 2, 1 } },  //0>>3
 		{ { 0, 0 }, { 2, 0 }, { -1, 0 }, { 2, -1 }, { -1, 2 } },  //1>>0
 		{ { 0, 0 }, { 1, 0 }, { -2, 0 }, { 1, 2 }, { -2, -1 } },  //2>>1
 		{ { 0, 0 }, { -2, 0 }, { 1, 0 }, { -2, 1 }, { 1, -2 } }   //3>>2
 };
-int8_t nextFigure = 0;
-int8_t actualFigure = 0;
-ColorBitfield actualFigureColor = {0x03ff};
-bool randomizeFigure = false;      //if create new random figure
-const int8_t tetris_game_width = 10;  //standard game width
-int8_t tetrisMode = TETRIS_STATIC;
-const int8_t fastMovementBlockDelay = 1;
-const int8_t movementBlockDelay = 20;
-int8_t actualMovementBlockDelay = movementBlockDelay;
-int8_t blockDelay = movementBlockDelay;
-int8_t startBlockDelay = movementBlockDelay;
-uint16_t tetrisScore = 0;
+static int8_t nextFigure = 0;
+static int8_t actualFigure = 0;
+static ColorBitfield actualFigureColor = {0x03ff};
+static bool randomizeFigure = false;      //if create new random figure
+static const int8_t tetris_game_width = 10;  //standard game width
+static int8_t tetrisMode = TETRIS_STATIC;
+static const int8_t fastMovementBlockDelay = 1;
+static const int8_t movementBlockDelay = 20;
+static const int16_t deadDelay = 3000;
+static int8_t actualMovementBlockDelay = movementBlockDelay;
+static int8_t blockDelay = movementBlockDelay;
+static int8_t startBlockDelay = movementBlockDelay;
+static uint16_t tetrisScore = 0;
+
+static bool previousNokiaFrameCopied = false;
+static bool previousHUB75FrameCopied = false;
 
 void TETRIS_Init(void) {
 	NOKIA_StartDataPrepare();
@@ -102,34 +109,133 @@ void TETRIS_Init(void) {
 	HUB75_Clear();
 	TETRIS_DrawBorder();
 
+	memset(tetrisPlayfield, 0, sizeof(tetrisPlayfield));
+
 	randomizeFigure = true;
 	nextFigure = randomNumber % TETRIS_NR_FIGURES;
-	tetrisMode = 0;
+	tetrisMode = TETRIS_STATIC;
 	tetrisScore = 0;
-
-	tetrisGameDelay = maxTetrisGameDelay;
-	moveTetrisLeftRightDelay = maxMoveTetrisLeftRightDelay;
 }
 
 void TETRIS_Logic(void) {
 	static uint32_t lastMillis = 0;
 	uint32_t actualMillis = HAL_GetTick();
 
+	previousNokiaFrameCopied = false;
+	previousHUB75FrameCopied = false;
+
 	//don't try to catch up if long time passed
 	if (actualMillis > (lastMillis + 2000)) lastMillis = actualMillis;
 
 	if (tetrisMode == TETRIS_STATIC) {
-		//if (SNAKE_CalculateDirection()) {
-		//	tetrisMode = TETRIS_PLAYING;
-		//}
+		if (GAMEPAD_GetHoldButton(LEFT) || GAMEPAD_GetHoldButton(RIGHT) || GAMEPAD_GetHoldButton(DOWN)) {
+			tetrisMode = TETRIS_PLAYING;
+		}
 	} else if (tetrisMode == TETRIS_PLAYING) {
+		if (GAMEPAD_GetHoldButton(DOWN)) {
+			if (blockDelay > fastMovementBlockDelay && startBlockDelay <= 0)
+				blockDelay = fastMovementBlockDelay;
+
+			actualMovementBlockDelay = fastMovementBlockDelay;
+		} else {
+			actualMovementBlockDelay = movementBlockDelay;
+		}
+
+		if (GAMEPAD_GetHoldButton(LEFT)) {
+			MoveTetrisLeftRight(-1, actualMillis);
+		} else if (GAMEPAD_GetHoldButton(RIGHT)) {
+			MoveTetrisLeftRight(1, actualMillis);
+		}
+
+		if (GAMEPAD_GetClickButton(A)) {
+			RotateTetrisFigure(-1);
+		}
+
+		if (GAMEPAD_GetClickButton(B)) {
+			RotateTetrisFigure(1);
+		}
 
 		while (actualMillis > lastMillis) {
-			lastMillis += tetrisGameDelay;
+			lastMillis += maxTetrisGameDelay;
+
+			NOKIA_StartDataPrepare();
+			if (!previousNokiaFrameCopied) {
+				NOKIA_CopyPreviousFrame();
+				previousNokiaFrameCopied = true;
+			}
+
+			if (HUB75_StartDrawing()) {
+				if (!previousHUB75FrameCopied) {
+					HUB75_CopyPreviousFrame();
+					previousHUB75FrameCopied = true;
+				}
+			}
+
+			if (randomizeFigure) {
+				actualFigure = nextFigure;
+				actualFigureColor = tetrisColors[actualFigure];
+				nextFigure = randomNumber % 7;
+				DrawAnyFigure(tetris_game_width + 3, 2, 0, nextFigure);  //draw another figure
+				figurePosX = figurePosXStart;
+				figurePosY = 0;
+				figureRot = 0;
+				randomizeFigure = false;
+				blockDelay = movementBlockDelay;
+				startBlockDelay = movementBlockDelay;
+				DrawFigure(figurePosX, figurePosY, -1);
+
+				if (!CheckFigurePossibility(figurePosX, figurePosY, figureRot)) {  //if spawning figure in another existing figure then it means it is end of the game
+					tetrisMode = TETRIS_DEAD;
+					lastMillis = actualMillis + deadDelay;
+					break;
+				}
+			}
+
+			if (blockDelay > 0)
+				blockDelay--;
+
+			if (startBlockDelay > 0)
+				startBlockDelay--;
+
+			if (blockDelay <= 0 && tetrisMode == TETRIS_PLAYING) {
+				for (int8_t i = 0; i < 4; i++) {
+					uint8_t figureBlockPos = GetFigureBlockPos(i, -10, -10, -1, -1);
+					if (figureBlockPos + TETRIS_WIDTH >= TETRIS_PIXEL_SIZE || tetrisPlayfield[figureBlockPos + TETRIS_WIDTH].color != 0) {  //check if figure is outside game area or figure is inside another figure when getting down
+						randomizeFigure = true;
+						break;
+					}
+				}
+
+				if (!randomizeFigure) {
+					figurePosY++;  //down movemnt of figure
+					DrawFigure(figurePosX, figurePosY - 1, -1);
+					if (startBlockDelay > 0)
+						blockDelay = movementBlockDelay;
+					else
+						blockDelay = actualMovementBlockDelay;
+				} else {  //if figure can't move then save info about position in array and check full lines for scoring
+					uint8_t minHeight = 0;
+					uint8_t maxHeight = 0;
+
+					for (uint8_t i = 0; i < 4; i++) {
+						uint8_t figureBlockPos = (figurePosY + (figures[actualFigure][figureRot][i] / 4)) * TETRIS_WIDTH + figurePosX + (figures[actualFigure][figureRot][i] % 4);
+						tetrisPlayfield[figureBlockPos] = actualFigureColor;
+
+						if (i == 0)
+							maxHeight = figureBlockPos / TETRIS_WIDTH;
+						if (i == 3)
+							minHeight = figureBlockPos / TETRIS_WIDTH;
+					}
+					CheckWholeLines(minHeight, maxHeight);
+				}
+			}
 		}
 	} else if (tetrisMode == TETRIS_DEAD) {
-		while (actualMillis > lastMillis) {
-			lastMillis += tetrisGameDelay;
+		if (actualMillis > lastMillis) {
+			lastMillis = actualMillis - 1;
+			if (GAMEPAD_GetHoldButton(LEFT) || GAMEPAD_GetHoldButton(RIGHT) || GAMEPAD_GetHoldButton(DOWN)) {
+				TETRIS_Init();
+			}
 		}
 	}
 }
@@ -142,17 +248,17 @@ static uint8_t GetFigureBlockPos(uint8_t i, int8_t myFigPosX, int8_t myFigPosY, 
 	return (myFigPosY + (figures[thisFigure][myFigRot][i] / 4)) * TETRIS_WIDTH + myFigPosX + (figures[thisFigure][myFigRot][i] % 4);
 }
 
-void DrawFigure(int8_t lastPosX, int8_t lastPosY, int8_t lastRot) {
+static void DrawFigure(int8_t lastPosX, int8_t lastPosY, int8_t lastRot) {
 	if (lastRot == -1) lastRot = figureRot;
 
 	for (int8_t i = 0; i < 4; i++) {
 		int8_t figureBlockPos = GetFigureBlockPos(i, lastPosX, lastPosY, lastRot, -1);
-		//ColorSingle(figureBlockPos, black, 100);
+		TETRIS_DrawPixel(figureBlockPos % TETRIS_WIDTH, figureBlockPos / TETRIS_WIDTH, black, false);
 	}
 
 	for (int8_t i = 0; i < 4; i++) {
 		int8_t figureBlockPos = GetFigureBlockPos(i, figurePosX, figurePosY, -1, -1);
-		//ColorSingle(figureBlockPos, actualFigureColor, 20);
+		TETRIS_DrawPixel(figureBlockPos % TETRIS_WIDTH, figureBlockPos / TETRIS_WIDTH, actualFigureColor, true);
 	}
 }
 
@@ -164,22 +270,22 @@ static void DrawAnyFigure(int8_t myFigPosX, int8_t myFigPosY, int8_t myFigRot, i
 
 	for (int8_t i = 0; i < 4; i++) {  //paint black 4 x 4 square
 		for (int8_t j = 0; j < 4; j++) {
-			//ColorSingle((myFigPosY + i) * TETRIS_WIDTH + myFigPosX + j, black, 100);
+			TETRIS_DrawPixel(myFigPosX + j, myFigPosY + i, black, false);
 		}
 	}
 
 	for (int8_t i = 0; i < 4; i++) {
 		uint8_t figureBlockPos = GetFigureBlockPos(i, myFigPosX, myFigPosY, myFigRot, thisFigure);
-		//ColorSingle(figureBlockPos, tetrisColors[thisFigure], 20);
+		TETRIS_DrawPixel(figureBlockPos % TETRIS_WIDTH, figureBlockPos / TETRIS_WIDTH, tetrisColors[thisFigure], true);
 	}
 }
 
 static bool CheckFigurePossibility(int8_t myFigPosX, int8_t myFigPosY, int8_t myFigRot) {
 	for (int8_t i = 0; i < 4; i++) {
 		uint8_t figureBlockPos = GetFigureBlockPos(i, myFigPosX, myFigPosY, myFigRot, -1);
-		//if ((figureBlockPos % TETRIS_WIDTH) < 0 || (figureBlockPos % TETRIS_WIDTH) >= TETRIS_WIDTH || (figureBlockPos / TETRIS_WIDTH) >= TETRIS_HEIGHT || snakeSgt[figureBlockPos][0] != 0) {  //check if figure is outside game area or figure is inside another figure
-		//	return false;
-		//}
+		if (figureBlockPos < 0 || (figureBlockPos / TETRIS_WIDTH) >= TETRIS_HEIGHT || tetrisPlayfield[figureBlockPos].color != 0) {  //check if figure is outside game area or figure is inside another figure
+			return false;
+		}
 	}
 	return true;
 }
@@ -255,28 +361,44 @@ static void RotateTetrisFigure(int8_t dir)  //1 - clockwise  -1 - counter clockw
 	}
 }
 
-static void MoveTetrisLeftRight(int8_t dir) {
-	while (moveTetrisLeftRightDelay >= maxMoveTetrisLeftRightDelay) {
-		moveTetrisLeftRightDelay -= maxMoveTetrisLeftRightDelay;
+static void MoveTetrisLeftRight(int8_t dir, uint32_t actualMillis) {
+	static uint32_t lastMillis = 0;
+
+	//don't try to catch up if long time passed
+	if (actualMillis > (lastMillis + 2000)) lastMillis = actualMillis - 1;
+
+	while (actualMillis > lastMillis) {
+		lastMillis += maxMoveTetrisLeftRightDelay;
+
 		if (tetrisMode == TETRIS_PLAYING) {
 			bool canMove = true;
 
 			for (int8_t i = 0; i < 4; i++) {
 				uint8_t figureBlockPos = GetFigureBlockPos(i, -10, -10, -1, -1);
-				//if ((figureBlockPos % TETRIS_WIDTH) + dir < 0 || (figureBlockPos % TETRIS_WIDTH) + dir >= TETRIS_WIDTH || snakeSgt[figureBlockPos + dir][0] != 0) {  //check if figure is outside game area or figure is inside another figure
-				//	canMove = false;
-				//	break;
-				//}
+				if ((figureBlockPos % TETRIS_WIDTH) + dir < 0 || (figureBlockPos % TETRIS_WIDTH) + dir >= TETRIS_WIDTH || tetrisPlayfield[figureBlockPos + dir].color != 0) {  //check if figure is outside game area or figure is inside another figure
+					canMove = false;
+					break;
+				}
 			}
 
 			if (canMove) {
 				figurePosX += dir;
-				DrawFigure(figurePosX - dir, figurePosY, -1);
+				NOKIA_StartDataPrepare();
+				if (!previousNokiaFrameCopied) {
+					NOKIA_CopyPreviousFrame();
+					previousNokiaFrameCopied = true;
+				}
+
+				if (HUB75_StartDrawing()) {
+					if (!previousHUB75FrameCopied) {
+						HUB75_CopyPreviousFrame();
+						previousHUB75FrameCopied = true;
+					}
+					DrawFigure(figurePosX - dir, figurePosY, -1);
+				}
 			}
 		}
 	}
-
-	//moveTetrisLeftRightDelay += calcMillis;
 }
 
 static void CheckWholeLines(int8_t minHeight, int8_t maxHeight) {
@@ -286,18 +408,18 @@ static void CheckWholeLines(int8_t minHeight, int8_t maxHeight) {
 	for (uint8_t i = 0; i < iterations; i++) {
 		bool scorePoints = true;
 		for (uint8_t j = 0; j < TETRIS_WIDTH; j++) {
-			//if (snakeSgt[actHeight * TETRIS_WIDTH + j][0] == 0) {
-			//	scorePoints = false;
-			//	actHeight--;
-			//	break;
-			//}
+			if (tetrisPlayfield[actHeight * TETRIS_WIDTH + j].color == 0) {
+				scorePoints = false;
+				actHeight--;
+				break;
+			}
 		}
 
 		if (scorePoints) {
 			for (int8_t k = actHeight - 1; k >= 0; k--) {
 				for (uint8_t l = 0; l < TETRIS_WIDTH; l++) {
-					//snakeSgt[(k + 1) * TETRIS_WIDTH + l][0] = snakeSgt[k * TETRIS_WIDTH + l][0];
-					//ColorSingle((k + 1) * TETRIS_WIDTH + l, colors[snakeSgt[k * TETRIS_WIDTH + l][0]], 20);
+					tetrisPlayfield[(k + 1) * TETRIS_WIDTH + l] = tetrisPlayfield[k * TETRIS_WIDTH + l];
+					TETRIS_DrawPixel(l, (k + 1), tetrisPlayfield[k * TETRIS_WIDTH + l], tetrisPlayfield[k * TETRIS_WIDTH + l].color > 0);
 				}
 			}
 
